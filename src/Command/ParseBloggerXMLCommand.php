74a8e0a4f7dc5f0bb6ca1f3021d74a8436b776f3
Wakonda <amatukami@hotmail.fr>
2022-10-19 22:49:53 +0200

    Webdirectory macro

---
+++ src/Command/ParseBloggerXMLCommand.php
@@ -0,0 +1,223 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Doctrine\ORM\EntityManagerInterface;
+
+use App\Entity\FileManagement;
+use App\Entity\Grimoire;
+use App\Entity\Language;
+use App\Entity\State;
+use App\Entity\SurThemeGrimoire;
+
+class ParseBloggerXMLCommand extends Command
+{
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+		parent::__construct();
+        $this->em = $em;
+    }
+
+    // the name of the command (the part after "bin/console")
+    protected static $defaultName = 'app:parse-blogger-xml';
+
+    protected function configure()
+    {
+        // ...
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+		$output->writeln("Start Event migration");
+
+		$conn = $this->em->getConnection();
+
+		$blogId = "4192778394306065291";
+
+		$dom = new \DOMDocument();
+		$dom->loadXML(file_get_contents((new Grimoire())->getTmpUploadRootDir()."blog-09-25-2022.xml"));
+
+		$entries = $dom->getElementsByTagName('entry');
+
+		$categoriesArray = [];
+
+		$i = 0;
+
+		foreach ($entries as $entry) {	
+			$categories = $entry->getElementsByTagName("category");
+
+			if(!$this->isPost($categories))
+				continue;
+			
+			if(strpos($entry->getElementsByTagName("id")->item(0)->nodeValue, "tag:blogger.com,1999:blog-${blogId}.post") !== false) {
+				$idsArray = explode("-", $entry->getElementsByTagName("id")->item(0)->nodeValue);
+				
+				$entity = $this->em->getRepository(Grimoire::class)->getGrimoireBySocialNetworkIdentifiers("Blogger", end($idsArray));
+				// dd($entity);
+				if(empty($entity))
+					$entity = new Grimoire();
+				
+				$this->em->persist($entity);
+				
+				$language = $this->em->getRepository(Language::class)->findOneBy(["abbreviation" => "fr"]);
+				
+				$entity->setLanguage($language);
+				
+				$entity->setTitle($entry->getElementsByTagName("title")->item(0)->nodeValue);
+				$entity->setWritingDate(new \DateTime($entry->getElementsByTagName("published")->item(0)->nodeValue));
+				$entity->setPublicationDate(new \DateTime($entry->getElementsByTagName("updated")->item(0)->nodeValue));
+				
+				$links = $entry->getElementsByTagName("link");
+				
+				$sourceArray = [[
+					"author" => null,
+					"url" => $links->item($links->length - 1)->getAttribute("href"),
+					"type" => "url",
+				]];
+				
+				$entity->setSource(json_encode($sourceArray));
+				
+				$imgData = [];
+				
+				$domContent = new \DOMDocument();
+				
+				libxml_use_internal_errors(true);
+				$domContent->loadHTML(mb_convert_encoding($entry->getElementsByTagName("content")->item(0)->nodeValue, 'HTML-ENTITIES', 'UTF-8'));
+				libxml_clear_errors();
+				
+				$images = $domContent->getElementsByTagName("img");
+				
+				if(!empty($images->item(0))) {
+					$imgArray = explode("/", $images->item(0)->getAttribute("src"));
+					$imgData = ["title" => $imgArray[count($imgArray) - 1], "content" => file_get_contents($images->item(0)->getAttribute("src"))];
+				}
+				
+				$imgs = array();
+				foreach($images as $img) {
+					$imgs[] = $img;
+				}
+				foreach($imgs as $img) {
+					$img->parentNode->removeChild($img);
+				}
+				
+				$spans = $domContent->getElementsByTagName("span");
+				
+				foreach($spans as $span) {
+					$span->removeAttribute("style");
+				}
+				
+				$divs = $domContent->getElementsByTagName("div");
+				
+				foreach($divs as $div) {
+					$div->removeAttribute("style");
+					$div->removeAttribute("align");
+				}
+				
+				foreach($divs as $div) {
+					if($div->getAttribute("class") == "separator")
+						$div->parentNode->removeChild($div);
+				}
+				
+				$brs = $domContent->getElementsByTagName("br");
+				
+				foreach($brs as $br) {
+					$br->parentNode->removeChild($br);
+				}
+				
+				$html = $domContent->saveHTML();
+		
+				$html = preg_replace('~<(?:!DOCTYPE|/?(?:html|body))[^>]*>\s*~i', '', $html);
+
+				$html = preg_replace('/(<!--)([\s\S]*?)-->/', '', $html);
+				$html = preg_replace('/<(\w+)\b(?:\s+[\w\-.:]+(?:\s*=\s*(?:"[^"]*"|"[^"]*"|[\w\-.:]+))?)*\s*\/?>\s*<\/\1\s*>/', '', $html);
+				$html = preg_replace('/^(<br\s*\/?>)*|(<br\s*\/?>)*$/i', '', $html);
+
+				$entity->setText($html);
+				
+				$currentCategories = [
+				  'Amour' => 'love',
+				  'Argent' => 'job',
+				  'Bonheur' => 'happiness',
+				  'Chance' => 'job',
+				  'Couple / Famille' => 'family',
+				  'Don' => 'ability',
+				  'Exorcisme' => 'exorcism',
+				  'Guérison' => 'healing',
+				  'Prière' => 'faith',
+				  'Prière à Marie' => 'faith',
+				  'Travail' => 'job',
+				  'Défunt / mort' => 'death',
+				  'Protection' => 'protection'
+				];
+				
+				$categoryArray = [];
+				
+				foreach($categories as $category) {
+					$term = $category->getAttribute("term");
+					if(!filter_var($term, FILTER_VALIDATE_URL)) {
+						$c = $term;
+					}
+					
+					$categoryArray[] = $term;
+				}
+				// dump($c);
+				$stg = $this->em->getRepository(SurThemeGrimoire::class)->findOneBy(["internationalName" => $currentCategories[$c].'Catholicism', "language" => $language]);
+				
+				if(!empty($stg))
+					$entity->setSurTheme($stg);
+				
+				$state = $this->em->getRepository(State::class)->findOneBy(["internationalName" => "Validate", "language" => $language]);
+				
+				$entity->setState($state);
+				$entity->setPseudoUsed("Wakonda");
+				$entity->setArchive(false);
+
+				if(!empty($imgData)) {
+					$illustration = $entity->getIllustration();
+					
+					if(empty($illustration))
+						$illustration = new FileManagement();
+					
+					$illustration->setTitleFile($imgData["title"]);
+					$illustration->setRealNameFile($imgData["title"]);
+					$illustration->setLicense("Pixabay");
+					$illustration->setUrlSource("https://pixabay.com/");
+					$illustration->setExtensionFile(pathinfo($imgData["title"], PATHINFO_EXTENSION));
+					
+					file_put_contents($entity->getTmpUploadRootDir().$imgData["title"], $imgData["content"]);
+					
+					$entity->setIllustration($illustration);
+					
+					$this->em->persist($illustration);
+				}
+
+				$socialNetworkIdentifiers = [];
+				$socialNetworkIdentifiers["Blogger"] = [
+					"id" => end($idsArray),
+					"url" => $links->item($links->length - 1)->getAttribute("href"),
+					"labels" => $categoryArray,
+				];
+				
+				$entity->setSocialNetworkIdentifiers($socialNetworkIdentifiers);
+			}
+		}
+		
+		$this->em->flush();
+
+        return 0;
+    }
+	
+	private function isPost($categories): bool 
+	{
+		foreach($categories as $category)
+			if(parse_url($category->getAttribute("term"), PHP_URL_FRAGMENT) == "post")
+				return true;
+
+		return false;
+	}
+}
---
+++ src/Command/SocialNetworkIdentifiersFormatCommand.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Doctrine\ORM\EntityManagerInterface;
+
+use App\Entity\Book;
+use App\Entity\Cartography;
+use App\Entity\Document;
+use App\Entity\EventMessage;
+use App\Entity\Movies\Movie;
+use App\Entity\Movies\TelevisionSerie;
+use App\Entity\Photo;
+use App\Entity\Testimony;
+use App\Entity\Video;
+use App\Entity\Grimoire;
+use App\Entity\News;
+use App\Entity\Stores\Store;
+use App\Entity\Licence;
+
+class SocialNetworkIdentifiersFormatCommand extends Command
+{
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+		parent::__construct();
+        $this->em = $em;
+    }
+
+    // the name of the command (the part after "bin/console")
+    protected static $defaultName = 'app:social-network-identifiers-format';
+
+    protected function configure()
+    {
+        // ...
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+		$output->writeln("Start Event migration");
+
+		$conn = $this->em->getConnection();
+		
+		
+		
+		$datas = $this->em->getRepository(Licence::class)->findAll();
+		
+		foreach($datas as $data)
+		{
+			preg_match('/^CC-BY[A-Z-]*|^CC0/', $data->getTitle(), $matches);
+		
+			if(empty($matches))
+				continue;
+	
+			if($matches[0] == "CC0")
+				$title = "CC0 1.0";
+			else
+				$title = $matches[0]." 3.0";
+			
+			$data->setTitle($title);
+			$data->setInternationalName($title);
+
+			$this->em->persist($data);
+		}
+		
+		$this->em->flush();
+die("ooo");
+		$urlId = [
+			"jakin-boaz" => "4192778394306065291",
+			"bookoflucifer" => "8587307742034849671",
+			"prieres-et-sortileges" => "1611333979864196065",
+			"amatukami" => "3616068588689105914",
+			"wakonda666" => "976120769055861867",
+			"activite-paranormale" => "6843544030232757764",
+			"thetempleofzebuleon" => "3619394577589453859",
+			"elgrimoriodeastaroth" => "6143285371855196758",
+			"testap7" => "2865018866226462436"
+		];
+		
+		$tables = [
+			Book::class,
+			Cartography::class,
+			Document::class,
+			EventMessage::class,
+			Movie::class,
+			TelevisionSerie::class,
+			Photo::class,
+			Testimony::class,
+			Video::class,
+			Grimoire::class,
+			News::class,
+			Store::class
+		];
+
+		foreach($tables as $table) {
+			$qb = $this->em->createQueryBuilder();
+
+			$datas = $qb->select("e")
+			   ->from($table, "e")
+			   ->where("e.socialNetworkIdentifiers IS NOT NULL")
+			   ->getQuery()->getResult();
+
+			foreach($datas as $data) {
+				$d = $data->getSocialNetworkIdentifiers();
+				
+				if(!in_array(key($d["Blogger"]), array_values($urlId))) {
+					
+					if(!isset($d["Blogger"]["url"]))
+						continue;
+					
+					$urlArray = explode(".", parse_url($d["Blogger"]["url"], PHP_URL_HOST));
+					$v = ["Blogger" => [$urlId[reset($urlArray)] => $d["Blogger"]]];
+					
+					$data->setSocialNetworkIdentifiers($v);
+					
+					$this->em->persist($data);
+				}
+			}
+
+			$this->em->flush();
+		}
+
+        return 0;
+    }
+	
+	private function isPost($categories): bool 
+	{
+		foreach($categories as $category)
+			if(parse_url($category->getAttribute("term"), PHP_URL_FRAGMENT) == "post")
+				return true;
+
+		return false;
+	}
+}
