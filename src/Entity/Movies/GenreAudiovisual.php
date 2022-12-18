51c971aca1731a13cfc7e3247c4ffb14b81b881c
Wakonda <amatukami@hotmail.fr>
2022-12-18 11:31:35 +0100

    Genre audiovisual issue illustration

--- src/Entity/Movies/GenreAudiovisual.php
+++ src/Entity/Movies/GenreAudiovisual.php
@@ -27,8 +27,7 @@ class GenreAudiovisual
     private $id;
 
     /**
-	 * Assert\File(maxSize="6000000")
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $photo;
 
@@ -126,45 +125,6 @@ class GenreAudiovisual
     public function getTmpUploadRootDir() {
         // the absolute directory path where uploaded documents should be saved
         return __DIR__ . '/../../../public/'.$this->getAssetImagePath();
-    }
-
-    /**
-     * @ORM\PrePersist()
-     * @ORM\PreUpdate()
-     */
-    public function uploadPhoto() {
-        // the file property can be empty if the field is not required
-        if (null === $this->photo) {
-            return;
-        }
-
-		if(is_object($this->photo))
-		{
-			$NameFile = basename($this->photo->getClientOriginalName());
-			$reverseNF = strrev($NameFile);
-			$explodeNF = explode(".", $reverseNF, 2);
-			$NNFile = strrev($explodeNF[1]);
-			$ExtFile = strrev($explodeNF[0]);
-			$NewNameFile = uniqid().'-'.$NNFile.".".$ExtFile;
-			if(!$this->id){
-				$this->photo->move($this->getTmpUploadRootDir(), $NewNameFile);
-			}else{
-				if (is_object($this->photo))
-					$this->photo->move($this->getUploadRootDir(), $NewNameFile);
-			}
-			if (is_object($this->photo))
-				$this->setPhoto($NewNameFile);
-		} elseif(filter_var($this->photo, FILTER_VALIDATE_URL)) {
-			$parser = new \App\Service\APParseHTML();
-			$html = $parser->getContentURL($this->photo);
-			$pi = pathinfo($this->photo);
-			$extension = $res = pathinfo(parse_url($this->photo, PHP_URL_PATH), PATHINFO_EXTENSION);
-			$filename = preg_replace('#\W#', '', $pi["filename"]).".".$extension;
-			$filename = uniqid()."_".$filename;
-
-			file_put_contents($this->getTmpUploadRootDir().$filename, $html);
-			$this->setPhoto($filename);
-		}
     }
 
     /**
