344715bc2ae87b04a3c04063b0b4e7ae6b317480
Wakonda <amatukami@hotmail.fr>
2023-10-15 17:53:54 +0200

    Random product action

--- src/Controller/StoreController.php
+++ src/Controller/StoreController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 use App\Entity\Stores\Store;
 use App\Form\Type\StoreSearchType;
@@ -56,10 +57,30 @@ class StoreController extends AbstractController
 		));
 	}
 
-    public function randomAction(Request $request, EntityManagerInterface $em)
+    public function randomAction(Request $request, EntityManagerInterface $em, TranslatorInterface $translator)
     {
-		$entity = $em->getRepository(Store::class)->getRandom($request->getLocale(), [Store::BOOK_CATEGORY]);
-		
-        return new Response($entity->getimageEmbeddedCode());
+		$category = explode("|", $request->query->get("category", Store::BOOK_CATEGORY));
+		$locale = $request->query->get("locale", $request->getLocale());
+
+		$entity = $em->getRepository(Store::class)->getRandom($locale, $category);
+
+		if(empty($entity))
+			return new Response();
+
+		$media = $entity->getimageEmbeddedCode();
+
+		if(empty($entity->getimageEmbeddedCode()))
+			$media = "<img src='".$request->getUriForPath('/extended/photo/store/'.$entity->getPhoto())."'>";
+
+		$text = "<div style='text-align: center'>".$media."</div>";
+		$text .= "<br>";
+		if(Store::ALIEXPRESS_PLATFORM == $entity->getPlatform())
+			$text .= '<div style="text-align: center"><a href="'.$entity->getUrl().'" style="border: 1px solid #E52F20; padding: 0.375rem 0.75rem;background-color: #E52F20;border-radius: 0.25rem;color: black !important;text-decoration: none;">'.$translator->trans('store.index.BuyOnAliexpress', [], 'validators', $locale).'</a></div>';
+		elseif(Store::AMAZON_PLATFORM == $entity->getPlatform())
+			$text .= '<div style="text-align: center"><a href="'.$entity->getExternalAmazonStoreLink().'" style="border: 1px solid #ff9900; padding: 0.375rem 0.75rem;background-color: #ff9900;border-radius: 0.25rem;color: black !important;text-decoration: none;">'.$translator->trans('store.index.BuyOnAmazon', [], 'validators', $locale).'</a></div>';
+		elseif(Store::SPREADSHOP_PLATFORM == $entity->getPlatform())
+			$text .= '<div style="text-align: center"><a href="'.$entity->getUrl().'" style="border: 1px solid #a73c9e; padding: 0.375rem 0.75rem;background-color: #a73c9e;border-radius: 0.25rem;color: white !important;text-decoration: none;">'.$translator->trans('store.index.BuyOnSpreadshop', [], 'validators', $locale).'</a></div>';
+	
+        return new Response($text);
     }
 }
