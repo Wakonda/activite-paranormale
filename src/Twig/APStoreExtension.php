fa9b6e219abfae8caaacaf9a95e3aa29385ecbd2
Wakonda <amatukami@hotmail.fr>
2024-01-14 19:36:56 +0100

    Meta image for store show template

--- src/Twig/APStoreExtension.php
+++ src/Twig/APStoreExtension.php
@@ -5,6 +5,8 @@
 	use Twig\TwigFilter;
 	use Twig\TwigFunction;
 	use Doctrine\ORM\EntityManagerInterface;
+	use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+	use Symfony\Component\HttpFoundation\RequestStack;
 	
 	use App\Service\Currency;
 	use App\Entity\Stores\Store;
@@ -12,11 +14,7 @@
 
 	class APStoreExtension extends AbstractExtension
 	{
-		private $em;
-		
-		public function __construct(EntityManagerInterface $em)
-		{
-			$this->em = $em;
+		public function __construct(private EntityManagerInterface $em, private UrlGeneratorInterface $router, private RequestStack $requestStack) {
 		}
 
 		public function getFilters()
@@ -29,8 +27,10 @@
 		public function getFunctions()
 		{
 			return [
-				new TwigFunction('get_store_by_entity', array($this, 'getStoreByEntity')),
-				new TwigFunction('image_embedded_code', array($this, 'getImageEmbeddedCodeByEntity'))
+				new TwigFunction('get_store_by_entity', [$this, 'getStoreByEntity']),
+				new TwigFunction('image_embedded_code', [$this, 'getImageEmbeddedCodeByEntity']),
+				new TwigFunction('image_store', [$this, 'getImageEntity'])
+				
 			];
 		}
 
@@ -55,12 +55,30 @@
 			return null;
 		}
 
+		public function getImageEntity($entity) {
+			if(!empty($photo = $entity->getPhoto())) {
+				$baseurl = $this->requestStack->getCurrentRequest()->getSchemeAndHttpHost().$this->requestStack->getCurrentRequest()->getBasePath();
+				return $baseurl."/".$entity->getAssetImagePath().$photo;
+			} elseif($html = $entity->getImageEmbeddedCode()) {
+				$dom = new \DOMDocument;
+				@$dom->loadHTML($html);
+				$imgTag = $dom->getElementsByTagName('img')->item(0);
+
+				if ($imgTag) {
+					$src = $imgTag->getAttribute('src');
+					return $src;
+				} else
+					return null;
+			}
+
+			return null;
+		}
+
 		private function getSymbolByCurrency($currency) {
 			return Currency::getSymbolByCurrency($currency);
 		}
 
-		public function getName()
-			{
-				return 'ap_storeextension';
-			}
+		public function getName() {
+			return 'ap_storeextension';
 		}
+	}
