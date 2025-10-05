33ecf99adf2e17ddfcf914daf6f5d022d151b9ee
Wakonda <amatukami@hotmail.fr>
2025-10-05 10:22:23 +0200

    Routes replacement

--- src/Controller/TagWordAdminController.php
+++ src/Controller/TagWordAdminController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,10 +17,7 @@ use App\Entity\FileManagement;
 use App\Form\Type\TagWordAdminType;
 use App\Service\ConstraintControllerValidator;
 
-/**
- * TagWord controller.
- *
- */
+#[Route('/admin/grimoire')]
 class TagWordAdminController extends AdminGenericController
 {
 	protected $entityName = 'TagWord';
@@ -46,18 +44,21 @@ class TagWordAdminController extends AdminGenericController
 	{
 	}
 
-    public function indexAction()
+	#[Route('/', name: 'TagWord_Admin_Index')]
+    public function index()
     {
 		$twig = 'tags/TagWordAdmin/index.html.twig';
 		return $this->indexGeneric($twig);
     }
-	
-    public function showAction(EntityManagerInterface $em, $id)
+
+	#[Route('/{id}/show', name: 'TagWord_Admin_Show')]
+    public function show(EntityManagerInterface $em, $id)
     {
 		$twig = 'tags/TagWordAdmin/show.html.twig';
 		return $this->showGeneric($em, $id, $twig);
     }
 
+	#[Route('/new', name: 'TagWord_Admin_New')]
     public function newAction(Request $request, EntityManagerInterface $em)
     {
 		$formType = TagWordAdminType::class;
@@ -66,8 +67,9 @@ class TagWordAdminController extends AdminGenericController
 		$twig = 'tags/TagWordAdmin/new.html.twig';
 		return $this->newGeneric($request, $em, $twig, $entity, $formType);
     }
-	
-    public function createAction(Request $request, EntityManagerInterface $em, ConstraintControllerValidator $ccv, TranslatorInterface $translator)
+
+	#[Route('/create', name: 'TagWord_Admin_Create', requirements: ['_method' => "post"])]
+    public function create(Request $request, EntityManagerInterface $em, ConstraintControllerValidator $ccv, TranslatorInterface $translator)
     {
 		$formType = TagWordAdminType::class;
 		$entity = new TagWord();
@@ -75,29 +77,33 @@ class TagWordAdminController extends AdminGenericController
 		$twig = 'tags/TagWordAdmin/new.html.twig';
 		return $this->createGeneric($request, $em, $ccv, $translator, $twig, $entity, $formType);
     }
-	
-    public function editAction(EntityManagerInterface $em, $id)
+
+	#[Route('/{id}/edit', name: 'TagWord_Admin_Edit')]
+    public function edit(EntityManagerInterface $em, $id)
     {
 		$formType = TagWordAdminType::class;
 
 		$twig = 'tags/TagWordAdmin/edit.html.twig';
 		return $this->editGeneric($em, $id, $twig, $formType);
     }
-	
-	public function updateAction(Request $request, EntityManagerInterface $em, ConstraintControllerValidator $ccv, TranslatorInterface $translator, $id)
+
+	#[Route('/{id}/update', name: 'TagWord_Admin_Update', requirements: ['_method' => "post"])]
+	public function update(Request $request, EntityManagerInterface $em, ConstraintControllerValidator $ccv, TranslatorInterface $translator, $id)
     {
 		$formType = TagWordAdminType::class;
 		
 		$twig = 'tags/TagWordAdmin/edit.html.twig';
 		return $this->updateGeneric($request, $em, $ccv, $translator, $id, $twig, $formType);
     }
-	
+
+	#[Route('/{id}/delete', name: 'TagWord_Admin_Delete')]
     public function deleteAction(EntityManagerInterface $em, $id)
     {
 		return $this->deleteGeneric($em, $id);
     }
 
-	public function indexDatatablesAction(Request $request, EntityManagerInterface $em, TranslatorInterface $translator)
+	#[Route('/datatables', name: 'TagWord_Admin_IndexDatatables', requirements: ['_method' => "get"])]
+	public function indexDatatables(Request $request, EntityManagerInterface $em, TranslatorInterface $translator)
 	{
 		$informationArray = $this->indexDatatablesGeneric($request, $em);
 		$output = $informationArray['output'];
@@ -118,17 +124,20 @@ class TagWordAdminController extends AdminGenericController
 		return new JsonResponse($output);
 	}
 
-	public function showImageSelectorColorboxAction()
+	#[Route('/showImageSelectorColorbox', name: 'TagWord_Admin_ShowImageSelectorColorbox')]
+	public function showImageSelectorColorbox()
 	{
 		return $this->showImageSelectorColorboxGeneric('TagWord_Admin_LoadImageSelectorColorbox');
 	}
-	
-	public function loadImageSelectorColorboxAction(Request $request, EntityManagerInterface $em)
+
+	#[Route('/loadImageSelectorColorbox', name: 'TagWord_Admin_LoadImageSelectorColorbox')]
+	public function loadImageSelectorColorbox(Request $request, EntityManagerInterface $em)
 	{
 		return $this->loadImageSelectorColorboxGeneric($request, $em);
 	}
-	
-	public function autocompleteAction(Request $request, EntityManagerInterface $em)
+
+	#[Route('/autocomplete', name: 'TagWord_Admin_Autocomplete')]
+	public function autocomplete(Request $request, EntityManagerInterface $em)
 	{
 		$query = $request->query->get("q", null);
 		$locale = $request->query->get("locale", null);
@@ -148,8 +157,9 @@ class TagWordAdminController extends AdminGenericController
 
         return new JsonResponse($results);
 	}
-	
-    public function internationalizationAction(Request $request, EntityManagerInterface $em, $id)
+
+	#[Route('/internationalization/{id}', name: 'TagWord_Admin_Internationalization')]
+    public function internationalization(Request $request, EntityManagerInterface $em, $id)
     {
 		$formType = TagWordAdminType::class;
 		$entity = new TagWord();
